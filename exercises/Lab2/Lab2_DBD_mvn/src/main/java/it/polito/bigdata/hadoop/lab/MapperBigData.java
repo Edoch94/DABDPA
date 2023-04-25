dd23abdff277c00ed7674a6f6ab13ca3eb9e00b2
edoch <edo_chio@hotmail.it>
2023-04-25 23:12:04 +0200

    lab1 and lab2 done

---
+++ exercises/Lab2/Lab2_DBD_mvn/src/main/java/it/polito/bigdata/hadoop/lab/MapperBigData.java
@@ -0,0 +1,40 @@
+package it.polito.bigdata.hadoop.lab;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import it.polito.bigdata.hadoop.lab.DriverBigData.COUNTERS;
+
+/**
+ * Lab  - Mapper
+ */
+
+/* Set the proper data types for the (key,value) pairs */
+class MapperBigData extends Mapper<
+                    Text, 			// Input key type
+                    Text,    // Input value type
+                    Text,         	// Output key type
+                    Text> {	// Output value type
+    
+	String prefix;
+	
+	protected void setup(Context context) {
+		prefix = context.getConfiguration().get("initialLetters").toString();
+	}
+	
+    protected void map(
+		Text key,   			// Input key type
+		Text value,      // Input value type
+        Context context) throws IOException, InterruptedException {
+	
+		if(key.toString().startsWith(prefix)) {
+            context.write(key, value);
+            context.getCounter(COUNTERS.SELECTED_WORDS).increment(1);
+		} else {
+			context.getCounter(COUNTERS.DISCARDED_WORDS).increment(1);
+		}
+    }
+}
